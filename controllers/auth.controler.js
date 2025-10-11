45b1baa65626ed51ba325e4bee91f74be29e510c
mahfujulhoque12 <mdmahfujulhoque123@gmail.com>
2025-10-11 22:30:11 +0600

    add-signin

--- controllers/auth.controler.js
+++ controllers/auth.controler.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-export const signup = async (req, res) => {
+import { errorHandaler } from "./../utils/error";
+export const signup = async (req, res, next) => {
   try {
     const { userName, email, password } = req.body;
     const hashedPassword = bcrypt.hashSync(password, 10);
@@ -11,7 +12,20 @@ export const signup = async (req, res) => {
       user: newUser,
     });
   } catch (error) {
-    console.error("Signup error:", error);
-    res.status(500).json({ message: "Server error", error: error.message });
+    // console.error("Signup error:", error);
+    // res.status(500).json({ message: "Server error", error: error.message });
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validuser = await User.findOne({ email });
+    if (!validuser) return next(errorHandaler(404, "user not found"));
+    const validPassword = bcrypt.compareSync(password, validuser.password);
+    if (!validPassword) return next(errorHandaler(401, "Wrong Crendtail"));
+  } catch (error) {
+    next(error);
   }
 };
--- index.js
+++ index.js
@@ -3,17 +3,24 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
-
+import cors from "cors";
 dotenv.config();
 
 // Connect to MongoDB
 connectDB();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", userRouter);
 app.use("/api", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  return res.status(statusCode).json({ success: false, statusCode, message });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
