2a561b093e82b718c0ad6051b2ec66e8ad859792
mahfujulhoque12 <mdmahfujulhoque123@gmail.com>
2025-10-26 20:18:09 +0600

    fix-sign-in

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000", 
+    credentials: true, // 🔥 allow cookies
+  })
+);
 app.use(express.json());
 
 app.use("/api", userRouter);
